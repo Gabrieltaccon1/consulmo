1a297e1e3ec037c7301648f0428c424bd02a4ca3
Gabriel Bernardes Tacconi Lourenço <gabrieltacconi147@gmail.com>
2019-06-17 16:05:50 -0300

    consulmo

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace gasto_combustivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int tempogastoemHoras, velocidadeMedia, distancia;
+
+            double quantidadedeLitros;
+
+            Console.WriteLine("DIGITE O TEMPO GASTO NA VIAGEM EM HORAS: ");
+            tempogastoemHoras = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE A VELOCIDADE EM KM/H: ");
+            velocidadeMedia = int.Parse(Console.ReadLine());
+
+
+            distancia = tempogastoemHoras * velocidadeMedia;
+
+            quantidadedeLitros = distancia / 12.0;
+
+            Console.WriteLine("O VALOR GASTO EM COMBUSTIVEL É DE: " + quantidadedeLitros.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
